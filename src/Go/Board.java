0a0eaa8f5ca7acf382731f5c0cf6d444811faf3e
teddyrkt <teddyrakotoarivelo@gmail.com>
2023-12-11 20:13:22 +0100

    V9

--- src/Go/Board.java
+++ src/Go/Board.java
@@ -1,12 +1,17 @@
 package Go;
-public class Board { // ok
+
+public class Board {
     private char[][] plateau;
     private int taille;
     private static final String alphabet = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
-
+    private char joueurActif;
+    private int passesBlancs = 0;
+    private int passesNoirs = 0;
+  
     public Board(int size) {
         this.taille = size;
         this.plateau = new char[taille][taille];
+        this.joueurActif = 'W';
 
         for (int i = 0; i < taille; i++) {
             for (int j = 0; j < taille; j++) {
@@ -14,28 +19,165 @@ public class Board { // ok
             }
         }
     }
- 
+
     public void showBoard() {
-        System.out.print("    "); 
+    	System.out.println();
+    	System.out.println("WHITE (O) has captured 0 stones");
+    	System.out.println("BLACK (X) has captured 0 stones");
+    	System.out.println();
+        System.out.print("   ");        
         for (int i = 0; i < taille; i++) {
             System.out.print(alphabet.charAt(i) + " ");
-           
         }
         System.out.println("    ");
         for (int i = taille - 1; i >= 0; i--) {
             int indiceLateral = i + 1;
-            System.out.print((indiceLateral < 10 ? " " : "") + indiceLateral + "  ");
-            
+            System.out.print((indiceLateral < 10 ? " " : "") + indiceLateral + " ");
+
             for (int j = 0; j < taille; j++) {
                 System.out.print(plateau[i][j] + " ");
             }
-            System.out.print((indiceLateral < 10 ? " " : "") + indiceLateral); 
-            System.out.println();
+            if (taille <10)
+            	System.out.print("" + indiceLateral);
+            else
+            	System.out.print((indiceLateral < 10 ? " " : "") + indiceLateral);
+            	System.out.println();
         }
-        System.out.print("    ");
+        System.out.print("   ");
         for (int i = 0; i < taille; i++) {
             System.out.print(alphabet.charAt(i) + " ");
         }
         System.out.println("    ");
     }
+    
+    
+    public boolean play(String move) {
+        String[] parts = move.split(" ");
+
+        if (parts.length < 2 || parts.length > 3) {
+            System.out.println("Illegal move");
+            return false;
+        }
+        String color;
+        String vertex;
+
+        if (parts.length == 3 && parts[0].equalsIgnoreCase("move")) {
+            color = parts[1].toLowerCase();
+            vertex = parts[2];
+        } else {
+            color = parts[0].toLowerCase();
+            vertex = parts[1];
+        }
+
+        //System.out.println("Color: " + color + ", Vertex: " + vertex);
+
+        if (!(color.equals("white") || color.equals("w") || color.equals("black") || color.equals("b"))) {
+            return false;
+        }
+        
+
+        if (!isValidVertex(vertex)) {
+            return false;
+        }
+
+        char player = (color.equals("white") || color.equals("w")) ? 'W' : 'B';
+
+        if (!isCorrectPlayer(player)) {
+            return false;
+        }
+        
+        if (!vertex.equals("pass")) {
+            int[] coordinates = getCoordinates(vertex);
+            //System.out.println("Coordinates: (" + coordinates[0] + ", " + coordinates[1] + ")");
+
+            if (!move(coordinates[0], coordinates[1])) {
+                return false;
+            }
+            passesBlancs = 0;
+            passesNoirs = 0;
+        } else {
+            if (player == 'W') {
+                passesBlancs++;
+            } else {
+                passesNoirs++;
+            }
+            if ((player == 'W' && passesBlancs == 2) || (player == 'B' && passesNoirs == 2)) {
+                endGame();
+                return true;
+            }
+        }
+
+        switchPlayer();
+        return true;
+    }
+
+
+    private boolean move(int row, int col) {
+        if (row < 0 || row >= taille || col < 0 || col >= taille || plateau[row][col] != '.') {
+            return false;
+        }
+        if (getActivePlayer()=='W')
+        	plateau[row][col] = 'O';
+        else
+        	plateau[row][col] = 'X';
+        return true;
+    }
+    
+
+    private boolean isValidVertex(String vertex) {
+        return vertex.matches("[a-zA-Z](1[0-9]|[1-9])|pass");
+    }
+    
+
+    private int[] getCoordinates(String vertex) {
+        int[] coordinates = new int[2];
+        if (vertex.equals("pass")) {
+            coordinates[0] = -1;  
+            coordinates[1] = -1;
+        } else {
+            coordinates[0] = Integer.parseInt(vertex.substring(1)) - 1;
+            
+            coordinates[1] = alphabet.indexOf(Character.toUpperCase(vertex.charAt(0)));
+        }
+        return coordinates;
+    }
+    
+    
+    private void switchPlayer() {
+        joueurActif = (joueurActif == 'W') ? 'B' : 'W';
+    }
+
+   
+    public char getActivePlayer() {
+        return joueurActif;
+    }
+    
+    private boolean isCorrectPlayer(char player) {
+        return player == joueurActif;
+    }
+    
+
+    private void endGame() {
+    	if (getActivePlayer()=='W')
+    		System.out.println("Game over. Black wins!");
+    	else
+    		System.out.println("Game over. White wins!");
+        System.exit(0);
+    }
+    
+    
+    public String getAlphabet() {
+        return alphabet;
+    }
+    
+    public void clearBoard() {
+        for (int i = taille - 1; i >= 0; i--) {
+            for (int j = 0; j < taille; j++) {
+            	plateau[i][j]='.';
+            }
+        }
+        passesBlancs = 0;
+        passesNoirs = 0;
+    }
+     
 }
--- src/IHM/test.java
+++ src/IHM/test.java
@@ -1,62 +1,87 @@
 package IHM;
+
+import java.util.Arrays;
 import java.util.Scanner;
 import Go.Board;
 
 public class test {
-    public static void main(String[] args) {
-        Board go = null;  
-        while (true) {
-            Scanner entry = new Scanner(System.in);
-            String entre2 = entry.nextLine();
-            String[] entree = entre2.split(" ");
-            
+    private static int currentInstructionNumber = 1;
 
-            if (entree[0].equals("quit")) {
-                entry.close();
-                System.exit(0);
-            } else if (entree[0].equals("boardsize")) {
-                if (entree.length == 2) {
-                    go = new Board(Integer.valueOf(entree[1]));
-
-                }
-            } else if (entree[0].equals("showboard") && go != null) {
-                go.showBoard();
-            }
-        }
-    } 
-}
-
-
-
-
-/*
-package board;
-import java.util.Scanner;
-
-public class test {
     public static void main(String[] args) {
         Board go = null;
         Scanner entry = new Scanner(System.in);
+
         while (true) {
             String input = entry.nextLine();
-            String[] inputParts = input.split(" ");
-            
-            if (inputParts.length == 2 && inputParts[1].equals("boardsize")) {
-                int size = Integer.parseInt(inputParts[0]);
-                if(size >=4 && size<=25) {
-                	go = new Board(size);
-                }
-                else {
-                	System.out.println("Taille incorrect");
-                }
-            } else if (inputParts[0].equals("showboard") && go != null) {
-                go.showBoard();
-            } else if (inputParts[0].equals("quit")) {
+            String[] commandParts = input.split(" ");
+            int instructionNumber = -1;
+
+            if (commandParts.length > 0 && commandParts[0].matches("\\d+")) {
+                instructionNumber = Integer.parseInt(commandParts[0]);
+                commandParts = Arrays.copyOfRange(commandParts, 1, commandParts.length);
+            } else {
+                instructionNumber = -1;
+            }
+
+            if (instructionNumber != -1 && instructionNumber == currentInstructionNumber) {
+                currentInstructionNumber++;
+            }
+
+            if (commandParts[0].equals("quit")) {
+            	printResult(instructionNumber, true, "");
                 entry.close();
                 System.exit(0);
+                
+            } else if (commandParts[0].equals("boardsize")) {       
+                if (commandParts.length == 2) {
+                    int newSize = Integer.valueOf(commandParts[1]);
+                    if (newSize < 2 || newSize > 25) {
+                        printResult(instructionNumber, false, "Unacceptable size");
+                    } else {
+                        go = new Board(newSize);
+                        printResult(instructionNumber, true, "");
+                    }
+                }   
+            }
+  
+             else if (commandParts[0].equals("showboard") && go != null) {
+            	printResult(instructionNumber, true, "");
+                go.showBoard();
+            } else if (commandParts[0].equals("play") && go != null) {
+                StringBuilder moveCommand = new StringBuilder("move");
+                for (int i = 1; i < commandParts.length; i++) {
+                    moveCommand.append(" ").append(commandParts[i]);
+                }
+                char playerColor = (commandParts[1].equalsIgnoreCase("white") || commandParts[1].equalsIgnoreCase("w")) ? 'W' : 'B';
+
+                if (playerColor != go.getActivePlayer()) {
+                    printResult(instructionNumber, false, "It's not your turn");
+                } else if (go.play(moveCommand.toString())) {
+                    printResult(instructionNumber, true, "");
+                } else {
+                    printResult(instructionNumber, false, "Illegal move");
+                }
+            } else if (commandParts[0].equals("clearboard") && go != null) {
+                go.clearBoard();
+                printResult(instructionNumber, true, "");
+            } else {
+                printResult(instructionNumber, false, "Unknown command");
+            }
+        }
+    }
+
+    private static void printResult(int instructionNumber, boolean success, String message) {
+        if (instructionNumber == -1) {
+            if (success==false) {
+                System.out.println(message);
+            }
+        }
+        else {
+            if (success) {
+                System.out.println("=" + instructionNumber);
+            } else {
+                System.out.println("?" + instructionNumber + " " + message);
             }
         }
     }
 }
-
-*/
